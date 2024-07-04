4116df88f5a4eb0b58ab423cce67da82b9251de6
Sami Khokher <112092098+Codeit88@users.noreply.github.com>
2024-07-05 01:00:40 +0500

    furst

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatsController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+     Route::get('/chat', [ChatsController::class, 'index']);
+     Route::get('messages', [ChatsController::class, 'fetchMessages']);
+    Route::post('messages', [ChatsController::class, 'sendMessage']);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
